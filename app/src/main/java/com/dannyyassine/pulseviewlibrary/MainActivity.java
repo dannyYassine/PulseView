be763e6332d24c161c66b9f5bf249ff0c1fcde9f
Danny Yassine <dannyyassine@gmail.com>
2016-08-21 19:03:10 -0400

    adding ui elments to customize demo

--- app/src/main/java/com/dannyyassine/pulseviewlibrary/MainActivity.java
+++ app/src/main/java/com/dannyyassine/pulseviewlibrary/MainActivity.java
@@ -2,18 +2,62 @@ package com.dannyyassine.pulseviewlibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Switch;
 
 import com.dannyyassine.pulseview.PulseView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mStartColor;
+    private EditText mEndColor;
+    private SeekBar mPeriod;
+    private Switch mIsRandom;
+    private SeekBar mMinValue;
+    private SeekBar mMaxValue;
+
+    private Button mUpdateButton;
+    private PulseView mPulseView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PulseView pulseView = (PulseView) this.findViewById(R.id.pulse_view);
-        pulseView.startPulse();
+
+        mStartColor = (EditText) this.findViewById(R.id.edit_text_start_color);
+        mEndColor = (EditText) this.findViewById(R.id.edit_text_end_color);
+        mPeriod = (SeekBar) this.findViewById(R.id.seekbar_period);
+        mIsRandom = (Switch) this.findViewById(R.id.switch_is_random);
+        mMinValue = (SeekBar) this.findViewById(R.id.seekbar_minimum);
+        mMaxValue = (SeekBar) this.findViewById(R.id.seekbar_maximum);
+
+        mMinValue.setBottom(0);
+        mMinValue.setMax(10000);
+
+        mMinValue.setBottom(0);
+        mMinValue.setMax(10000);
+
+        mPeriod.setMax(10000);
+
+        mUpdateButton = (Button) this.findViewById(R.id.button_update);
+        mUpdateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mPulseView.setMaxRandom(mMaxValue.getProgress());
+                mPulseView.setMinRandom(mMinValue.getProgress());
+                mPulseView.setRandom(mIsRandom.isChecked());
+                mPulseView.setPeriod(mPeriod.getProgress());
+
+            }
+        });
+
+        mPulseView = (PulseView) this.findViewById(R.id.pulse_view);
+        mPulseView.startPulse();
 
     }
 
--- pulseview/src/main/java/com/dannyyassine/pulseview/PulseView.java
+++ pulseview/src/main/java/com/dannyyassine/pulseview/PulseView.java
@@ -2,8 +2,11 @@ package com.dannyyassine.pulseview;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Handler;
 import android.support.annotation.ColorRes;
+import android.support.v4.content.res.ResourcesCompat;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,8 +30,80 @@ public class PulseView extends RelativeLayout {
     private Timer mTimer = new Timer();
 
     private long animationDuration;
-    private ColorRes startColor;
-    private ColorRes endColor;
+    private long mPeriod = 1000L;
+    private int mStartColor = -1;
+    private int mEndColor = -1;
+    private boolean mIsRandom = false;
+    private int mMinRandom = 1000;
+    private int mMaxRandom = 1000;
+
+    /**
+     * Getters
+     */
+
+    public int getEndColor() {
+        return mEndColor;
+    }
+
+    public int getStartColor() {
+        return mStartColor;
+    }
+
+    public long getPeriod() {
+        return mPeriod;
+    }
+
+    public boolean isRandom() {
+        return mIsRandom;
+    }
+
+    public long getAnimationDuration() {
+        return animationDuration;
+    }
+
+    public int getMinRandom() {
+        return mMinRandom;
+    }
+
+    public int getMaxRandom() {
+        return mMaxRandom;
+    }
+
+    /**
+     * Setters
+     */
+
+    public void setRandom(boolean random) {
+        mIsRandom = random;
+    }
+
+    public void setPeriod(long period) {
+        mPeriod = period;
+    }
+
+    public void setStartColor(int startColor) {
+        mStartColor = startColor;
+    }
+
+    public void setEndColor(int endColor) {
+        mEndColor = endColor;
+    }
+
+    public void setAnimationDuration(long animationDuration) {
+        this.animationDuration = animationDuration;
+    }
+
+    public void setMaxRandom(int maxRandom) {
+        mMaxRandom = maxRandom;
+    }
+
+    public void setMinRandom(int minRandom) {
+        mMinRandom = minRandom;
+    }
+
+    /**
+     *  Constructors
+     */
 
     public PulseView(Context context) {
         super(context);
@@ -50,30 +125,61 @@ public class PulseView extends RelativeLayout {
         TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
                 R.styleable.PulseView, 0, 0);
 
-         try {
+        try {
 
-         } finally {
-             a.recycle();
-         }
+            mStartColor = a.getColor(R.styleable.PulseView_pulse_start_color, -1);
+            mEndColor = a.getColor(R.styleable.PulseView_pulse_end_color, -1);
+            mPeriod = a.getInt(R.styleable.PulseView_pulse_period, (int)mPeriod);
+            mIsRandom = a.getBoolean(R.styleable.PulseView_pulse_is_random, false);
+            mMaxRandom = a.getInt(R.styleable.PulseView_pulse_high_random, mMaxRandom);
+            mMinRandom = a.getInt(R.styleable.PulseView_pulse_low_random, mMinRandom);
+
+        } finally {
+            a.recycle();
+        }
 
         View view = LayoutInflater.from(context).inflate(R.layout.pulse_view, this, true);
 
-        mImageView = (ImageView) view.findViewById(R.id.pulse);
-
+        mImageView = (ImageView) findViewById(R.id.pulse);
+        
     }
 
     public void startPulse() {
 
+        if (mPeriod < 0) {
+            assert true; // mPeriod can't be negative
+        }
+
         mTimer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
+
                 final PulseImageView imageView = new PulseImageView(getContext());
+                GradientDrawable pulseDrawable = (GradientDrawable) ResourcesCompat.getDrawable(getResources(), R.drawable.pulse, null);
+
+                if (mStartColor != -1 && mEndColor != -1) {
+                    try {
+                        int[] colors = {mStartColor, mEndColor};
+                        pulseDrawable.setColors(colors);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
                 imageView.setImageResource(R.drawable.pulse);
                 imageView.setLayoutParams(new ViewGroup.LayoutParams(mImageView.getWidth(), mImageView.getHeight()));
                 Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.pulse_anim);
-                long duration = new Random().nextInt(2000) + 1000;
+
+                long duration = mPeriod;
+                if (mIsRandom) {
+                    duration = new Random().nextInt(mMaxRandom) + mMinRandom;
+                }
+
                 animation.setDuration(duration);
+                animation.setFillEnabled(true);
+                animation.setFillAfter(true);
                 imageView.startAnimation(animation);
+
                 imageView.setPulseAnimationEndListener(new PulseImageView.PulseAnimationEndListener() {
                     @Override
                     public void onPulseAnimationEnd(final PulseImageView imageView) {
@@ -94,14 +200,13 @@ public class PulseView extends RelativeLayout {
                 });
 
             }
-        }, 0, 1000L);
+        }, 0, mPeriod);
 
     }
 
     private void stopPulse() {
         mTimer.cancel();
     }
-
 
 
 }
